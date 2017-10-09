e67e0019cc32065263978ae08df96ea84449cb9d
jengleberg <jeffengleberg@hotmail.com>
2017-10-09 11:49:34 -0600

    fixes movie.js

--- movie.js
+++ movie.js
@@ -1,15 +1,21 @@
 'use strict'
 
-const request = require('request');
-const url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCBnq_jGi2RW6neA_sLlPn8RwdN4AKp-D4&cx=000886761285250742499:wtmhnmzut4a&q=';
+function get(film) {
 
-const get = function(movieTitle) {
+const request = require('request');
+const url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCBnq_jGi2RW6neA_sLlPn8RwdN4AKp-D4&cx=000886761285250742499:wtmhnmzut4a&q=' + film;
+
+
 	request(url,  function(error, response, body) {
-	console.log(url + JSON.parse(body).title);
+	console.log(url + JSON.parse(body).items[0].snippet);
 
 	});
 
-};
+	request(url, function(error, response, body) {
+		console.log(JSON.parse(body).items[0].link);
+	});
+
+}
 
 
 module.exports = get;
